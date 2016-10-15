166405f1b20b687f1a58d1b9397b976c1c8c8f7f
Zenobius Jiricek <airtonix@gmail.com>
2016-10-15 19:21:14 +1030

    Fix: expose Context through vuex

---
+++ src/store/context/index.js
@@ -0,0 +1,12 @@
+export default {
+  state: {
+    ...Context
+  },
+  actions: {},
+  getters: {
+  	isDev: state => state.ENV === state.ENV_NAME__DEV,
+  	isProd: state => state.ENV === state.ENV_NAME__PROD,
+  },
+  mutations: {}
+};
+
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import Vue from 'vue';
 
 Vue.use(Vuex);
 
+import context from './context';
 import pages from './pages';
 import navigation from './navigation';
 
 export default new Vuex.Store({
   strict: true,
   modules: {
+    context,
     pages,
     navigation,
   }
