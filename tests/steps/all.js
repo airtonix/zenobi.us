c0bff5fca5accac69e788791fd3487bc33c33f77
Zenobius Jiricek <airtonix@gmail.com>
2016-10-22 23:30:13 +1030

    Fix: update documentation for tests/support/selectors

--- tests/steps/all.js
+++ tests/steps/all.js
@@ -28,18 +28,33 @@ define(function(require) {
           });
       })
 
+      /**
+       * Require an visible MDL card containing desired text in the header
+       * @param  {String} title  title in the card header
+       * @return Intern.Leadfoot.Command
+       */
       .then('there should be a card titled "$title"', function(title) {
         return this.remote
           .setFindTimeout(Constants.ELEMENT_TIMEOUT)
           .findByXpath(Selectors.cardWithTitle(title));
       })
 
+      /**
+       * Require a form field (found by it's label) to be visible
+       * @param  {String} label  text to be found in the fields label.
+       * @return Intern.Leadfoot.Command
+       */
       .define('And the field "$label" should be visible', function(label) {
         return this.remote
           .setFindTimeout(Constants.ELEMENT_TIMEOUT)
           .findByXpath(Selectors.fieldWithLabel(label));
       })
 
+      /**
+       * Require a button be visible containing text as its label.
+       * @param  {String} label  text to be found in the buttons label.
+       * @return Intern.Leadfoot.Command
+       */
       .define('And the "$label" button should be visible', function(label) {
         return this.remote
           .setFindTimeout(Constants.ELEMENT_TIMEOUT)
