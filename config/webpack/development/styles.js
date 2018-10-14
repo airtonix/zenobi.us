39ba6718c396e8fc41fa764c3437c2a9cba98e05
Zenobius Jiricek <airtonix@gmail.com>
2018-10-15 08:05:41 +1030

    Fix: move config out of src

---
+++ config/webpack/development/styles.js
@@ -0,0 +1,42 @@
+const Config = require('webpack-config').default;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = new Config()
+  .merge({
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ],
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          // { loader: 'postcss-loader',
+          //   options: {
+          //     ident: 'postcss',
+          //     sourceMap: true,
+          //     plugins: [
+          //       require('postcss-preset-env')({}),
+          //       require('postcss-functions')({}),
+          //     ]
+          //   }
+          // },
+          { loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }]
+    }
+  })
+;
