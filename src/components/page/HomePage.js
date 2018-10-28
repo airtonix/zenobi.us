c1b67e4ef71a867f8350eab9cd37e125555b4a14
Zenobius Jiricek <airtonix@gmail.com>
2018-10-28 12:52:36 +1030

    start gatsby

--- src/components/page/HomePage.js
+++ src/components/page/HomePage.js
@@ -1,7 +1,12 @@
+import React from 'react';
 import PageBase from './PageBase';
+
+import HeroCard from '@components/cards/hero-card';
 
 export default function HomePage (props) {
 	return <PageBase mixClass="home-page">
-		Home!
+		<HeroCard
+			title="zenobi.us"
+			subtitle="Web Developer, Vuejs, NodeJs and Docker"/>
 	</PageBase>
 }
--- src/content/index.md
+++
@@ -1,7 +0,0 @@
-import HeroCard from '@components/cards/hero-card';
-import Collection from '@components/collection';
-
-
-<HeroCard
-	title="zenobi.us"
-	subtitle="Node.Js and Docker Protaginist" />
