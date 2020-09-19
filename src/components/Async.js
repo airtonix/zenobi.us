1a92b404b3db4943766645f19ac124354bfc2c27
Zenobius Jiricek <airtonix@users.noreply.github.com>
2020-09-19 10:29:24 +0000

    cleanup

--- src/components/Async.js
+++ src/components/Async.js
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
+export default function asyncComponent (getComponent) {
 
-export default function asyncComponent(getComponent) {
-
-    class AsyncComponent extends Component {
+  class AsyncComponent extends Component {
         static Component = null;
         state = {
-            Component: AsyncComponent.Component
+          Component: AsyncComponent.Component
         };
 
         componentWillMount () {
-            if (!this.state.Component) {
-                getComponent().then(Component => {
-                    AsyncComponent.Component = Component;
-                    this.setState({ Component });
-                });
-            }
+          if (!this.state.Component) {
+            getComponent().then(Component => {
+              AsyncComponent.Component = Component;
+              this.setState({ Component });
+            });
+          }
         }
 
         render () {
-            const { Component } = this.state;
-            if (Component) {
-                return <Component {...this.props} />
-            }
-            return null;
+          const { Component } = this.state;
+          if (Component) {
+            return <Component {...this.props} />
+          }
+          return null;
         }
-    }
-    return AsyncComponent;
+  }
+
+  return AsyncComponent;
 }
