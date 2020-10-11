2bf86bca738a95ceb5a60b1ae37be029af115b39
Zenobius Jiricek <airtonix@users.noreply.github.com>
2020-10-12 09:25:57 +1030

    see if it prints something

--- tools/deploy/cleanup-production.js
+++ tools/deploy/cleanup-production.js
@@ -1,8 +1,12 @@
-const del = require('del')
-
 module.exports = async function (git) {
-  await del(
-    '**/*',
-    '!pr/**/*'
-  )
+  await git.rm('-n', './**', ':(exclude)pr/**/*');
+  await git.rm('./**', ':(exclude)pr/**/*');
 }
+
+// const del = require('del')
+// module.exports = async function (git) {
+//   await del(
+//     '**/*',
+//     '!pr/**/*'
+//   )
+// }
