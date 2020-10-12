74c156463b95231806a81034a707904b6bf7c6c4
Zenobius Jiricek <airtonix@users.noreply.github.com>
2020-10-12 10:34:06 +1030

    Updates

--- component---src-templates-default-tsx-cd80b17da9180d0f0224.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"4e3g":function(e,n,t){"use strict";t.r(n),t.d(n,"query",(function(){return u}));var a=t("q1tI"),r=t.n(a),c=t("A2+M"),d=t("3ffe"),o=t("Tadz");const u="1855600754";n.default=e=>{const{data:{allSitePage:{nodes:n},mdx:{body:t}}={}}=e,a=Object(o.a)(n);return r.a.createElement(d.b,{value:a},r.a.createElement(c.MDXRenderer,null,t))}}}]);
-//# sourceMappingURL=component---src-templates-default-tsx-cd80b17da9180d0f0224.js.map
